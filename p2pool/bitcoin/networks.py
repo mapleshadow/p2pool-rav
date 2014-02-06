86aece0f324d7c27611194de1d465dc3b6ef2613
mapleshadow <mapleshadow@gmail.com>
2014-02-06 17:32:10 +0800

    Add VertCoin

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -928,13 +928,38 @@ nets = dict(
         POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
         BLOCK_PERIOD=60, # s
         SYMBOL='RDD',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Reddcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Reddcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.reddcoin'), 'reddcoin.conf'),
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Reddcoin') 
+		if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Reddcoin/') 
+		if platform.system() == 'Darwin' else os.path.expanduser('~/.reddcoin'), 'reddcoin.conf'),
         BLOCK_EXPLORER_URL_PREFIX='http://altexplorer.net/block/',
         ADDRESS_EXPLORER_URL_PREFIX='http://altexplorer.net/address/',
         TX_EXPLORER_URL_PREFIX='http://altexplorer.net/tx/',
         SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0,
+    ),
+    vertcoin=math.Object(
+        P2P_PREFIX='fabfb5da'.decode('hex'),
+        P2P_PORT=5889,
+        ADDRESS_VERSION=71,
+        RPC_PORT=5888,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'vertcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//840000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('vtc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=150, # s
+        SYMBOL='VTC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Vertcoin') 
+		if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Vertcoin/') 
+		if platform.system() == 'Darwin' else os.path.expanduser('~/.vertcoin'), 'vertcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.vertcoin.org/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.vertcoin.org/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.vertcoin.org/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
     ),
 
 )
